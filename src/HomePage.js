2ab8694fd69278054a7f0e38a7a6be3b486f132b
Sang Jun Park <sangjunpark@Sangs-MacBook-Pro.local>
2017-10-28 22:01:32 -0400

    minor updates to login and homepage

---
+++ src/HomePage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        currentAction: "home"
+	  };
+  }
+	componentDidMount() {
+		document.body.classList.toggle('backgroundColor', true)
+	}
+
+	render() {
+		if (this.state.currentAction == "home") {
+			return (
+        <div className="App" className="divStyle">
+          <h1 style={{color: "white"}}>Welcome back</h1>
+          xyz...
+        </div>
+			);
+		}
+	}
+}
+
+export default HomePage;
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
 import MUIButton from 'muicss/lib/react/button';
+import Dropdown from 'muicss/lib/react/dropdown';
+import DropdownItem from 'muicss/lib/react/dropdown-item';
 
 class LoginPage extends Component {
   constructor(props) {
@@ -14,9 +16,15 @@ class LoginPage extends Component {
   componentDidMount() {
     document.body.classList.toggle('backgroundColor', true)
   }
+
   signup = () => {
     this.setState({
       currentAction: "signup"
+    });
+  }
+  resetPassword = () => {
+    this.setState({
+      currentAction: "resetPassword"
     });
   }
   back = () => {
@@ -24,23 +32,46 @@ class LoginPage extends Component {
       currentAction: "none"
     }); 
   }
+
   registerSignup = () => {}
   registerLogin = () => {}
+  registerResetPassword = () => {}
+
   render() {
     if (this.state.currentAction == "signup") {
       return (
         <div className="App">
           <div className="divStyle">
             <h1 style={{color: "white"}}>Create an Account</h1>
-            <input type="text" placeholder="Name" className="textField"/>
+            <input type="text" placeholder="First name" className="textField"/>
+            <input type="text" placeholder="Last name" className="textField"/>
             <input type="text" placeholder="Password" className="textField"/>
+            
+            <Dropdown color="primary" label="Gender" alignMenu="right">
+              <DropdownItem>Male</DropdownItem>
+              <DropdownItem>Female</DropdownItem>
+            </Dropdown>
+
+            <input type="text" placeholder="Birthdate" className="textField"/>
+            <input type="text" placeholder="Mobile number" className="textField"/>
             <input type="text" placeholder="Age" className="textField"/>
             <div>
-              etc...
+              xyz...
             </div>
+
+
             <MUIButton color="white" onClick={this.back}>Back</MUIButton>
             <MUIButton color="white" onClick={this.registerSignup}>Sign up</MUIButton>
           </div>
+        </div>
+      );
+    } else if (this.state.currentAction == "resetPassword") {
+      return (
+        <div className="App" className="divStyle">
+          <h1 style={{color: "white"}}>Recover your TypePhil account</h1>
+          <input type="text" placeholder="name@example.com" className="textField"/>
+          <MUIButton color="white" onClick={this.back}>Back</MUIButton>
+          <MUIButton color="white" onClick={this.registerResetPassword}>Reset password</MUIButton>
         </div>
       );
     } else {
@@ -49,8 +80,11 @@ class LoginPage extends Component {
           <h1 style={{color: "white"}}>Welcome to Typephil!</h1>
           <input type="text" placeholder="Username" className="textField"/>
           <input type="text" placeholder="Password" className="textField"/>
-          <MUIButton color="white" onClick={this.login}>Login</MUIButton>
-          <MUIButton color="white" onClick={this.signup}>Sign up</MUIButton>
+          <div>
+            <MUIButton color="white" onClick={this.login}>Login</MUIButton>
+            <MUIButton color="white" onClick={this.signup}>Sign up</MUIButton>
+          </div>
+          <MUIButton color="white" onClick={this.resetPassword}>Forgot password?</MUIButton>
         </div>
       );
     }
