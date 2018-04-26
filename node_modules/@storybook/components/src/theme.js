15f80d87f663de82315c4a27aaa36c84be07e9a4
Neil Chen <neilwchen@gmail.com>
2018-04-26 19:12:42 -0400

    merge and ignore `node_modules`

--- node_modules/@storybook/components/src/theme.js
+++
@@ -1,6 +0,0 @@
-export const baseFonts = {
-  fontFamily:
-    '-apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Lucida Grande", "Arial", sans-serif',
-  color: '#444',
-  WebkitFontSmoothing: 'antialiased',
-};
