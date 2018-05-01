3262cc63bd6a151dcf06177c423d692fd5211b06
Neil Chen <neilwchen@gmail.com>
2018-05-01 16:44:58 -0400

    uncommit sass-cache and fix scss for popup on right panel

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -7,15 +7,16 @@ import Header from './components/header';
 import Dropdown from 'react-dropdown'
 import moment from 'moment'
 import 'react-dropdown/style.css'
-import './style/styles.scss';
-import './style/SignupPage.scss'
+import './style/styles.css';
+import './style/SignupPage.css'
 
 class SignupPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Default month and year. User updates these values later.
       month: moment().month(),
-      year: moment().year() // defaults; will be updated by user later
+      year: moment().year()
     };
   }
 
@@ -41,13 +42,13 @@ class SignupPage extends Component {
   setMonth = e => {
     const month = e.value;
     this.setState({ month });
-    this.refs.dropdown_days.forceUpdate();
+    this.refs.dddays.forceUpdate();
   }
 
   setYear = e => {
     const year = e.value;
     this.setState({ year });
-    this.refs.dropdown_days.forceUpdate();
+    this.refs.dddays.forceUpdate();
   }
 
   getDays = (m, y) => {
@@ -57,7 +58,6 @@ class SignupPage extends Component {
       days.push(daysInMonth);
       daysInMonth--;
     }
-    console.log(days);
     return days;
   }
 
@@ -70,6 +70,10 @@ class SignupPage extends Component {
             return index + (moment().year()-50);
           }).reverse();
     var days = this.getDays(this.state.month, this.state.year);
+    var schoolyears = Array.apply(null, {length: 10}).map(
+          function(_, index) {
+            return index + moment().year()
+          });
     const defaultOption = 0;
 
     if(isLoggedIn) {
@@ -86,7 +90,7 @@ class SignupPage extends Component {
                     <div className="left-panel">
                         <h1 className="title">Sign up - It's free!</h1>
                         <p>Join TypePhil to get personalized help with your typing education, whether you're already studying or starting anew. We'll save all of your progress.</p>
-                        <p>By signing up for TypePhil, you agree to our Terms of Use and Privacy Notice.</p>
+                        <p>By signing up for TypePhil, you agree to our <a href="https://app.termly.io/document/terms-of-use-for-website/b57ed416-1978-4739-b295-a2578c7bff00">Terms of Use and Privacy Notice.</a></p>
 
                         <div className="void">
                         </div>
@@ -102,7 +106,7 @@ class SignupPage extends Component {
                 <div className="column column-10">
                 </div>
 
-                <div className="column column-40">
+                <div className="column column-40 right-panel-container">
                     <div className="right-panel">
                         <div className="row">
                             <div className="column column-50">
@@ -122,9 +126,9 @@ class SignupPage extends Component {
                             <div className="column column-50">
                                 <h2>BIRTHDATE</h2>
                                 <div className="row dropdowns">
-                                    <Dropdown options={months} onChange={this.updateMonth} value={defaultOption} placeholder="Month"/>
-                                    <Dropdown options={days} onChange={this._onSelect} value={defaultOption} placeholder="Day" id="dropdown_days"/>
-                                    <Dropdown options={years} onChange={this.updateYear} value={defaultOption} placeholder="Year"/>
+                                    <Dropdown options={months} onChange={this.updateMonth} value={defaultOption} placeholder="Month" id="ddmonths"/>
+                                    <Dropdown options={days} onChange={this._onSelect} value={defaultOption} placeholder="Day" id="dddays"/>
+                                    <Dropdown options={years} onChange={this.updateYear} value={defaultOption} placeholder="Year" id="ddyears"/>
                                 </div>
                             </div>
                         </div>
@@ -132,11 +136,11 @@ class SignupPage extends Component {
                         <div className="row password">
                             <div className="column column-50">
                                 <h2>PASSWORD</h2>
-                                <input placeholder=""/>
+                                <input placeholder="" type="password"/>
                             </div>
                             <div className="column column-50">
                                 <h2>RE-TYPE PASSWORD</h2>
-                                <input placeholder=""/>
+                                <input placeholder="" type="password"/>
                             </div>
                         </div>
 
@@ -145,25 +149,32 @@ class SignupPage extends Component {
                         </div>
                         <div className="row gender-radios">
                                 <div className="column column-20"><input type="radio" name="gender" value="male"></input>Male</div>
-                                <div className="column column-20"><input type="radio" name="gender" value="female"></input>Female</div>
+                                <div className="column column-25"><input type="radio" name="gender" value="female"></input>Female</div>
                                 <div className="column column-20"><input type="radio" name="gender" value="other"></input>Other</div>
                         </div>
 
                         <div className="row occupation">
+                          <div className="column column-50">
                             <h2>I AM CURRENTLY...</h2>
-                        </div>
-                        <div className="row occupation-radios">
-                            <div className="column column-50">
-                                <span>
-                                  <div className="row"><input type="radio" name="occupation" value="student"></input>A student</div>
-                                </span>
-                                <span>
-                                  <div className="row"><input type="radio" name="occupation" value="employed"></input>Employed</div>
-                                </span>
-                                <span>
-                                  <div className="row"><input type="radio" name="occupation" value="unemployed"></input>Unemployed</div>
-                                </span>
+                            <div className="occupation-radios">
+                              <span>
+                                <div className="row"><input type="radio" name="occupation" value="student"></input>A student</div>
+                              </span>
+                              <span>
+                                <div className="row"><input type="radio" name="occupation" value="employed"></input>Employed</div>
+                              </span>
+                              <span>
+                                <div className="row"><input type="radio" name="occupation" value="unemployed"></input>Unemployed</div>
+                              </span>
                             </div>
+                          </div>
+
+                          <div className="column column-50">
+                            <h2>SCHOOL YEAR</h2>
+                            <div className="occupation-specify">
+                              <Dropdown options={schoolyears} onChange={this.updateSchoolYear} value={defaultOption} placeholder="Select from below"/>
+                            </div>
+                          </div>
                         </div>
 
                         <div className="row next">
