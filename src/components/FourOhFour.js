e26f4c1dc1a320df7422829966b69e10618eb03f
Cesar Ibarra <cfi2103@columbia.edu>
2018-07-04 19:45:00 -0700

    Adding 404, changes to auth reducer

---
+++ src/components/FourOhFour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FourOhFour = () => {
+    return (
+        <React.Fragment>
+            <h1>Where do you think you're going ;) </h1>
+        </React.Fragment>
+    )
+}
+
+export default FourOhFour;
