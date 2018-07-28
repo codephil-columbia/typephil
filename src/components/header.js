d0e1bd562c05790e0b82cf4c7cfecc463611e487
Neil Chen <neilwchen@gmail.com>
2018-07-28 12:35:59 -0400

    prevent endless re-render, fix logout in header

--- src/components/header.js
+++ src/components/header.js
@@ -7,30 +7,30 @@ import { store } from '../store.js';
 import { Link } from 'react-router-dom';
 import '../style/navbar.css'
 
-const Header = props => {
+const Header = (props) => {
     return (
         <nav className="navigation">
             <div className="">
                 <HeaderLeft/>
-                <HeaderRight links={props.links} isLoggedIn={props.isLoggedIn} username={props.username}/>
+                <HeaderRight links={props.links} isLoggedIn={props.isLoggedIn} username={props.username} dispatch={props.dispatchLogout}/>
             </div>
         </nav>
     )
-}
+};
 
-const HeaderLeft = _ => {
+const HeaderLeft = (_) => {
     return (
         <a href="/">
         <img className="navigation-title" src="images/universal/TypePhil_Header_Logo.svg"/>
         </a>
     )
-}
+};
 
-const HeaderRight = props => {
+const HeaderRight = (props) => {
     console.log(props);
     return (
         <ul className="navigation-list float-right nav-right-list">
-            <li className="navigation-item profile-bubble"><ProfileOptions username={props.username} /></li>
+            <li className="navigation-item profile-bubble"><ProfileOptions username={props.username} dispatch={props.dispatch}/></li>
             { props.links === undefined ? "" : props.links.map((link, i) => {
                 const routePath = `/${link.toLowerCase()}`;
                 return (
@@ -42,9 +42,9 @@ const HeaderRight = props => {
 
         </ul>
     )
-}
+};
 
-const ProfileOptions = props => {
+const ProfileOptions = (props) => {
   return (
     <div className="dropdown">
       <button className="dropbtn">{ props.username ? props.username.charAt(0) : '' }</button>
@@ -52,12 +52,16 @@ const ProfileOptions = props => {
 				<div> 
 					<Link to="/profile">My Account</Link>
 				</div>
-				<div onClick={store.dispatch(dispatchLogout())}>
+				<div onClick={props.dispatch}>
 					Log Out
 				</div>
 			</div>
     </div>
   )
-}
+};
 
-export default Header;
+const mapDispatchToProps = {
+	dispatchLogout,
+};
+
+export default connect(null, mapDispatchToProps)(Header);
