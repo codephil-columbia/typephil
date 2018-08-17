c3f1aa28eb5d5ded1e7c5663279beeb56705afdf
Neil Chen <neilwchen@gmail.com>
2018-08-17 15:25:24 +0800

    highlight lessons on hover

--- src/LessonsView.js
+++ src/LessonsView.js
@@ -61,7 +61,6 @@ class LessonsView extends Component {
               return (
                 <h4
                   className='lesson-view-text' 
-                  style={{color: 'black'}}
                   key={i} 
                   onClick={() => this.userDidChangeLesson(lesson)}>
                   {lesson.LessonName}
