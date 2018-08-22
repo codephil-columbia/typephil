acd289b20eac1a4b11bbfd206f01bf74d700771a
cesaribarra10 <cfi2103@columbia.edu>
2018-08-22 17:31:42 +0800

    Remove default image heights, set hardcoded heights to fit screens better
    Add max height and width

--- src/components/TutorialImage.js
+++ src/components/TutorialImage.js
@@ -3,7 +3,7 @@ import ReactSVG from 'react-svg'
 
 
 const TutorialImage = ({ path }) => {
-  return <ReactSVG path={path} />
+  return <ReactSVG path={path} svgStyle={{ maxHeight: 400, maxWidth: 800 }} />
 }
 
 export default TutorialImage;
