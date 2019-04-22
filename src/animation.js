47ce3de038bb9e5a3765a58bce8812b0458dded1
ilianac <idc2117@columbia.edu>
2019-04-21 20:00:15 -0400

    added three rockets instead of 1

--- src/animation.js
+++ src/animation.js
@@ -32,17 +32,43 @@ class Animation extends React.Component {
 
   componentDidMount() {
     const Box = styler(document.querySelector('.box'));
+    const Box2 = styler(document.querySelector('.box2'));
+    const Box3 = styler(document.querySelector('.box3'));
 
     tween({
-      from: {x:-1000, y:100},
+      from: {x:-2000, y:-250},
 
-      to: { x: 1000, y:100 },
-      duration: 3000,
+      to: { x: 1000, y:-250},
+      duration: 8000,
       //flip: Infinity,
       // elapsed: 500,
       loop: 5,
       // yoyo: 5
     }).start(Box.set);
+
+    tween({
+      from: {x:-3000, y: -25},
+
+      to: { x: 1000, y: -25},
+      duration: 10000,
+      //flip: Infinity,
+      // elapsed: 500,
+      loop: 5,
+      // yoyo: 5
+    }).start(Box2.set);
+
+    tween({
+      from: {x:-1000, y:200},
+
+      to: { x: 1000, y:200 },
+      duration: 12000,
+      //flip: Infinity,
+      // elapsed: 500,
+      loop: 5,
+      // yoyo: 5
+    }).start(Box3.set);
+
+
 
     setInterval(() => {
       this.setState({ isMoving: !this.state.isMoving });
@@ -55,8 +81,18 @@ class Animation extends React.Component {
       // <Box className="box" pose={isMoving ? 'left' : 'right'}> 
         // Hi <img height="42" width="42" src="./Meteor2.svg"/>
       // </Box>
+      <div>
       <div className="box">Hello
       <img height="auto" width="100%" src="./images/games/Meteor.svg"/>
+      </div>
+
+      <div className="box2">Hel
+      <img height="auto" width="100%" src="./images/games/Meteor.svg"/>
+      </div>
+
+      <div className="box3">He
+      <img height="auto" width="100%" src="./images/games/Meteor.svg"/>
+      </div>
       </div>
     );
   }
