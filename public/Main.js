e03b717fe96dc026436ab054a1a07609ddeac73e
sangjunp4rk <sp3394@columbia.edu>
2019-05-31 08:40:22 +0800

    fixed building

---
+++ public/Main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron')    
+const path = require('path')
+
+function createWindow () {   
+  // Create the browser window.     
+	win = new BrowserWindow(
+		{
+			width: 1200, 
+			height: 900,
+			minWidth: 1200,
+			minHeight: 900
+		}
+	) 
+       
+	// and load the index.html of the app.     
+	// win.loadFile('index.html')
+	//win.loadURL('http://localhost:3000/selectGames')   
+	win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+}   
+
+app.on('ready', createWindow)
