1391eced53b1f614baf0d5316b929619a49608e5
Cesar Ibarra <cfi2103@columbia.edu>
2019-07-08 15:01:43 -0700

    Add fetch hand imgs for key, still need to to add correct paths Add tests for fetching imgs For #109

--- src/services/__tests__/keyboardHighlight.test.js
+++ src/services/__tests__/keyboardHighlight.test.js
@@ -3,9 +3,19 @@ import {_setHandsForKeyPressed} from '../keyboardHighlight';
 describe('Keyboard hand highlighting', () => {
   test('Sets retrieves img for key', () => {
     const keyPressed = 'a';
-    const images = {'a': ['pathForLeft', 'pathForRight']};
-    const { leftHandImage, rightHandImage } = _setHandsForKeyPressed(keyPressed, images);
+    const imgs = new Map([
+      ['a', {leftHandImage:'pathForLeft', rightHandImage:'pathForRight'}],
+    ]);
+    const { leftHandImage, rightHandImage } = _setHandsForKeyPressed(keyPressed, imgs);
     expect(leftHandImage).toEqual('pathForLeft');
     expect(rightHandImage).toEqual('pathForRight');
   });
+  test('It returns empty string when key is not present', () => {
+    const keyNotPresent = 'a';
+    const { leftHandImage, rightHandImage } = _setHandsForKeyPressed(keyNotPresent, new Map([
+      ['b', {leftHandImage:'left', rightHandImage:'right'}]
+    ]));
+    expect(leftHandImage).toEqual('');
+    expect(rightHandImage).toEqual('');
+  })
 });
--- src/services/keyboardHighlight.js
+++ src/services/keyboardHighlight.js
@@ -3,83 +3,82 @@ export function setHandsForKeyPressed(keyPressed) {
   return _setHandsForKeyPressed(keyPressed, imageForCharacter);
 };
 
-export function _setHandsForKeyPressed(keyPressed, images) {
+export function _setHandsForKeyPressed(keyPressed, imgs) {
   // This should never happen since all possible keys should be 
   // registered in imgForCharacter
-  if (!(keyPressed in imageForCharacter)) {
-    return { leftHandImage:'', rightHandImage: ''};
+  if (!imgs.has(keyPressed)) {
+    return {leftHandImage:'', rightHandImage: ''};
   }
-
-  const [leftHandImage, rightHandImage] = images[keyPressed];
-  return { leftHandImage, rightHandImage };
+  const {leftHandImage, rightHandImage} = imgs.get(keyPressed);
+  return {leftHandImage, rightHandImage};
 };
 
 // The value is an array whose 0 index is the 
 // left hand's image path, and whose first index 
 // is right hand's image path.
 // TODO: remove test paths
-const imageForCharacter = {
-  "A": ['finger-1', 'finger-10'],
-  "a": ['finger-1', 'finger-10'],
-  "B": ['finger-1', 'finger-10'],
-  "b": ['finger-1', 'finger-10'],
-  "C": ['finger-1', 'finger-10'],
-  "c": ['finger-1', 'finger-10'],
-  "D": ['finger-1', 'finger-10'],
-  "d": ['finger-1', 'finger-10'],
-  "E": ['finger-1', 'finger-10'],
-  "e": ['finger-1', 'finger-10'],
-  "F": ['finger-1', 'finger-10'],
-  "f": ['finger-1', 'finger-10'],
-  "G": ['finger-1', 'finger-10'],
-  "g": ['finger-1', 'finger-10'],
-  "H": ['finger-1', 'finger-10'],
-  "h": ['finger-1', 'finger-10'],
-  "I": ['finger-1', 'finger-10'],
-  "i": ['finger-1', 'finger-10'],
-  "J": ['finger-1', 'finger-10'],
-  "j": ['finger-1', 'finger-10'],
-  "K": ['finger-1', 'finger-10'],
-  "k": ['finger-1', 'finger-10'],
-  "L": ['finger-1', 'finger-10'],
-  "l": ['finger-1', 'finger-10'],
-  "M": ['finger-1', 'finger-10'],
-  "m": ['finger-1', 'finger-10'],
-  "N": ['finger-1', 'finger-10'],
-  "n": ['finger-1', 'finger-10'],
-  "O": ['finger-1', 'finger-10'],
-  "o": ['finger-1', 'finger-10'],
-  "P": ['finger-1', 'finger-10'],
-  "p": ['finger-1', 'finger-10'],
-  "Q": ['finger-1', 'finger-10'],
-  "q": ['finger-1', 'finger-10'],
-  "R": ['finger-1', 'finger-10'],
-  "r": ['finger-1', 'finger-10'],
-  "S": ['finger-1', 'finger-10'],
-  "s": ['finger-1', 'finger-10'],
-  "T": ['finger-1', 'finger-10'],
-  "t": ['finger-1', 'finger-10'],
-  "U": ['finger-1', 'finger-10'],
-  "u": ['finger-1', 'finger-10'],
-  "V": ['finger-1', 'finger-10'],
-  "v": ['finger-1', 'finger-10'],
-  "W": ['finger-1', 'finger-10'],
-  "w": ['finger-1', 'finger-10'],
-  "X": ['finger-1', 'finger-10'],
-  "x": ['finger-1', 'finger-10'],
-  "Y": ['finger-1', 'finger-10'],
-  "y": ['finger-1', 'finger-10'],
-  "Z": ['finger-1', 'finger-10'],
-  "z": ['finger-1', 'finger-10'],
-  "&nbsp;": ['finger-1', 'finger-10'],
-  "comma": ['finger-1', 'finger-10'],
-  "period": ['finger-1', 'finger-10'],
-  "apostrophe": ['finger-1', 'finger-10'],
-  "semicolon": ['finger-1', 'finger-10'],
-  "f-slash": ['finger-1', 'finger-10'],
-  "questionmark": ['finger-1', 'finger-10'],
-  "exclamationmark": "finger-3, finger-10"
-}
+const imageForCharacter = new Map([
+  ["A": ['images/hands/Left_ring.png', 'images/hands/']],
+  ["a": ['images/hands/Left_pinky.svg', 'images/hands/']],
+  ["B": ['images/hands/', 'images/hands/0']],
+  ["b": ['images/hands/', 'images/hands/0']],
+  ["C": ['images/hands/', 'images/hands/0']],
+  ["c": ['images/hands/', 'images/hands/0']],
+  ["D": ['images/hands/', 'images/hands/0']],
+  ["d": ['images/hands/', 'images/hands/0']],
+  ["E": ['images/hands/', 'images/hands/0']],
+  ["e": ['images/hands/', 'images/hands/0']],
+  ["F": ['images/hands/', 'images/hands/0']],
+  ["f": ['images/hands/', 'images/hands/0']],
+  ["G": ['images/hands/', 'images/hands/0']],
+  ["g": ['images/hands/', 'images/hands/0']],
+  ["H": ['images/hands/', 'images/hands/0']],
+  ["h": ['images/hands/', 'images/hands/0']],
+  ["I": ['images/hands/', 'images/hands/0']],
+  ["i": ['images/hands/', 'images/hands/0']],
+  ["J": ['images/hands/', 'images/hands/0']],
+  ["j": ['images/hands/', 'images/hands/0']],
+  ["K": ['images/hands/', 'images/hands/0']],
+  ["k": ['images/hands/', 'images/hands/0']],
+  ["L": ['images/hands/', 'images/hands/0']],
+  ["l": ['images/hands/', 'images/hands/0']],
+  ["M": ['images/hands/', 'images/hands/0']],
+  ["m": ['images/hands/', 'images/hands/0']],
+  ["N": ['images/hands/', 'images/hands/0']],
+  ["n": ['images/hands/', 'images/hands/0']],
+  ["O": ['images/hands/', 'images/hands/0']],
+  ["o": ['images/hands/', 'images/hands/0']],
+  ["P": ['images/hands/', 'images/hands/0']],
+  ["p": ['images/hands/', 'images/hands/0']],
+  ["Q": ['images/hands/', 'images/hands/0']],
+  ["q": ['images/hands/', 'images/hands/0']],
+  ["R": ['images/hands/', 'images/hands/0']],
+  ["r": ['images/hands/', 'images/hands/0']],
+  ["S": ['images/hands/', 'images/hands/0']],
+  ["s": ['images/hands/', 'images/hands/0']],
+  ["T": ['images/hands/', 'images/hands/0']],
+  ["t": ['images/hands/', 'images/hands/0']],
+  ["U": ['images/hands/', 'images/hands/0']],
+  ["u": ['images/hands/', 'images/hands/0']],
+  ["V": ['images/hands/', 'images/hands/0']],
+  ["v": ['images/hands/', 'images/hands/0']],
+  ["W": ['images/hands/', 'images/hands/0']],
+  ["w": ['images/hands/', 'images/hands/0']],
+  ["X": ['images/hands/', 'images/hands/0']],
+  ["x": ['images/hands/', 'images/hands/0']],
+  ["Y": ['images/hands/', 'images/hands/0']],
+  ["y": ['images/hands/', 'images/hands/0']],
+  ["Z": ['images/hands/', 'images/hands/0']],
+  ["z": ['images/hands/', 'images/hands/0']],
+  ["&nbsp;": ['images/hands/', 'images/hands/0']],
+  ["comma": ['images/hands/', 'images/hands/0']],
+  ["period": ['images/hands/', 'images/hands/0']],
+  ["apostrophe": ['images/hands/', 'images/hands/0']],
+  ["semicolon": ['images/hands/', 'images/hands/0']],
+  ["f-slash": ['images/hands/', 'images/hands/0']],
+  ["questionmark": ['images/hands/', 'images/hands/0']],
+  ["exclamationmark": ["finger-3, images/hands/0"]]
+]);
 
 export const mapToSpecialKeys = {
   "~": "`",
